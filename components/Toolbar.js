56e7d0a4fff6c839a8ad0b305b3d697c2bd85220
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-06-27 22:52:42 +0100

    Global Toolbar

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,30 +1,8 @@
 import { styled } from '../stitches.config';
 
 export default styled('div', {
-  display: 'grid',
-  placeItems: 'stretch',
-  placeContent: 'stretch',
+  display: 'flex',
+  flexDirection: 'row',
+  marginBottom: '$md',
   gap: '$md',
-  variants: {
-    cols: {
-      1: {
-        gridTemplateColumns: '1fr',
-      },
-      2: {
-        gridTemplateColumns: '1fr 1fr',
-      },
-      3: {
-        gridTemplateColumns: '1fr 1fr 1fr',
-      },
-      asymmetric: {
-        gridTemplateColumns: '3fr minmax(35%, 1fr)',
-      },
-      auto: {
-        gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-      },
-    },
-  },
-  defaultVariants: {
-    cols: 'asymmetric',
-  },
 });

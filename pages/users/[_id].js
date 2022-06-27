a28b90d423d1ef6693d33c060d6d50963ab5783d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-06-27 23:51:25 +0100

    New layout for user detail

--- pages/users/[_id].js
+++ pages/users/[_id].js
@@ -5,6 +5,47 @@ import Button from '../../components/Button';
 import Table from '../../components/Table';
 import Loading from '../../components/Loading';
 import PageContainer from '../../components/PageContainer';
+import Toolbar from '../../components/Toolbar';
+import Group from '../../components/Group';
+
+const Grid = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+  borderRadius: '$md',
+  gap: '$md',
+});
+
+const GridCell = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '$md',
+  borderRadius: '$md',
+  backgroundColor: '$gray1',
+  gap: '$xs',
+  variants: {
+    clickable: {
+      true: {
+        cursor: 'pointer',
+        '&:hover': {
+          backgroundColor: '$gray4',
+        },
+      },
+    },
+  },
+});
+
+const Label = styled('p', {
+  fontSize: '12px',
+  fontWeight: '$medium',
+  textTransform: 'uppercase',
+  color: '$gray11',
+});
+
+const Value = styled('p', {
+  fontSize: '18px',
+  fontWeight: '$medium',
+  color: '$gray12',
+});
 
 export default function Users() {
   //
@@ -14,11 +55,35 @@ export default function Users() {
 
   const { data: user } = useSWR('/api/users/' + _id);
 
+  function handleEditUser() {}
+
+  function handleDeleteUser() {}
+
   return user ? (
     <PageContainer title={'Colaboradores › ' + user.name}>
-      <p>{user.name}</p>
-      <p>{user.role}</p>
-      <p>{user.pwd}</p>
+      <Toolbar>
+        <Button onClick={handleEditUser}>Editar Colaborador</Button>
+        <Button color={'danger'} onClick={handleDeleteUser}>
+          Apagar Colaborador
+        </Button>
+      </Toolbar>
+
+      <Group title={'Informações Gerais'}>
+        <Grid>
+          <GridCell>
+            <Label>Nome</Label>
+            <Value>{user.name || '-'}</Value>
+          </GridCell>
+          <GridCell>
+            <Label>Posição</Label>
+            <Value>{user.role || '-'}</Value>
+          </GridCell>
+          <GridCell>
+            <Label>Password</Label>
+            <Value>{user.pwd || '-'}</Value>
+          </GridCell>
+        </Grid>
+      </Group>
     </PageContainer>
   ) : (
     <Loading />

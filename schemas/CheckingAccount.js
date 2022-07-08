42ae50a0e5a36af2433dd91a2e4080d3e1830338
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-07-08 03:17:23 +0100

    Update Schema

--- schemas/CheckingAccount.js
+++ schemas/CheckingAccount.js
@@ -23,8 +23,11 @@ export default z.object({
     .length(2, { message: 'Tax Region must be exactly 2 characters long' })
     .regex(/^[A-Z]+$/i, { message: 'Tax Region must be only letters' })
     .transform((value) => value.toUpperCase()),
-  tax_number: z
-    .string({ message: 'Tax Number must be a string' })
-    .length(9, { message: 'Tax Number must be exactly 9 characters long' })
-    .regex(/^[0-9]*$/, { message: 'Tax Number must be only numbers' }),
+  tax_number: z.preprocess(
+    (value) => String(value),
+    z
+      .string()
+      .length(9, { message: 'Tax Number must be exactly 9 characters long' })
+      .regex(/^[0-9]*$/, { message: 'Tax Number must be only numbers' })
+  ),
 });

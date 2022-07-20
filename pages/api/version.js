44522cad800ae0790ca9e834d81090eb03cfbfec
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-07-20 11:02:09 +0100

    Remove requireAuth from /api/version/

--- pages/api/version.js
+++ pages/api/version.js
@@ -1,15 +1,14 @@
 import pjson from '../../package.json';
-import { requireAuth } from '@clerk/nextjs/api';
 
 /* * */
 /* GET LATEST APP VERSION */
 /* Explanation needed. */
 /* * */
 
-export default requireAuth(async (req, res) => {
+export default async function appVersion(req, res) {
   return new Promise((resolve, reject) => {
     res.statusCode = 200;
     res.send({ latest: pjson.version });
     resolve();
   });
-});
+}
